a9a5ef1cece6927c5b2d722f23a4367d9b228a88
Bruno Alfred <brunoalfred@Brunos-MacBook-Pro.local>
2022-08-28 19:31:01 +0300

    Fresh start

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>bukshelf</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/css/app.css')
+
+    <!-- Scripts -->
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+</head>
+
+<body class="antialiased">
+
+    @if (Route::has('login'))
+
+    @auth
+    <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+    @else
+    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+    @if (Route::has('register'))
+    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+    @endif
+    @endauth
+
+    @endif
+
+    <h1 x-data="{ message: 'I ❤️ Alpine' }" x-text="message"></h1>
+
+    <div class="bg-white">
+        <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <h2 class="sr-only">Products</h2>
+
+            <div class="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+
+                @for($i = 0; $i < 10; $i++) <div class="group relative bg-white border border-gray-200 rounded-lg flex flex-col overflow-hidden">
+                    <div class="aspect-w-3 aspect-h-4 bg-gray-200 group-hover:opacity-75 sm:aspect-none sm:h-96">
+                        <img src="https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg" alt="Front of plain black t-shirt." class="w-full h-full object-center object-cover sm:w-full sm:h-full">
+                    </div>
+                    <div class="flex-1 p-4 space-y-2 flex flex-col">
+                        <h3 class="text-sm font-medium text-gray-900">
+                            <a href="#">
+                                <span aria-hidden="true" class="absolute inset-0"></span>
+                                Basic Tee
+                            </a>
+                        </h3>
+                        <p class="text-sm text-gray-500">Look like a visionary CEO and wear the same black t-shirt every day.</p>
+                        <div class="flex-1 flex flex-col justify-end">
+                            <p class="text-sm italic text-gray-500">Black</p>
+                            <p class="text-base font-medium text-gray-900">$32</p>
+                        </div>
+                    </div>
+            </div>
+
+
+            @endfor
+
+            <!-- More products... -->
+        </div>
+    </div>
+
+
+    </div>
+
+
+</body>
+
+</html>
